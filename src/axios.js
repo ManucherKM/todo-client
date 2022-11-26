c07cd1529ffb292e6ff9a07d3d0403afe2486913
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-11-26 17:14:08 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "https://todo-beckend.vercel.app"
+    baseURL: "https://todobeckend.herokuapp.com/todos"
 })
 
 export default instance
