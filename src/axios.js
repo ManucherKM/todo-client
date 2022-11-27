df6f8d40e2414dc1582e7ce3c1ff3e333c08b02b
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-11-27 11:56:56 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,12 +3,12 @@ import axios from "axios"
 /*
     Создаем свой "дефолтный" axios для запросов на бекенд.
     Это позволяет напрямую обращаться к роутам api.
-    Прим. теперь вместо запроса: axios.get("https://todotestquestion.herokuapp.com/todos"),
+    Прим. теперь вместо запроса: axios.get("https://todo-beckend-production.up.railway.app/todos"),
     мы можем написать: axios.get("/todos")
 */
 
 const instance = axios.create({
-    baseURL: "https://todotestquestion.herokuapp.com"
+    baseURL: "https://todo-beckend-production.up.railway.app"
 })
 
 export default instance
